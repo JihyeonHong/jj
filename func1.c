ab55f715f06e6a2b1e1a8212e6d6c94a7ba047e4
Roh Jaehee <201501642@inu.ac.kr>
2017-11-27 18:20:00 +0900

    tnwjd

--- func1.c
+++ func1.c
@@ -23,11 +23,6 @@ void func1()
 	result=num[0]op[0]num[1]
 
 
-<<<<<<< HEAD
 	printf("%d %c %d %c %d = %d\n",num[0], op[0], num[1], op[1], num[2], result);
 
-=======
-	printf("%d fff %c %d %c %d = %d\n",num[0], op[0], num[1], op[1], num[2], result);
-	
->>>>>>> 11799250ae85cb403c852966860e95b2f987af40
 }
