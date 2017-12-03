bb94206f7581fa9e292e6326d06707cf3c61a486
Hong JiHyeon <201501668@inu.ac.kr>
2017-12-03 14:38:54 +0900

    function creavtion

--- func1.c
+++ func1.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void func1()
+void func1(int stage)
 {
-	srand(time(NUMLL));
-
-	int num[4];
-	for(int i=0; i<4; i++)
+	srand(time(NUMLL)); //random_number
+	//malloc 2*number+3
+	int num[20];
+	for(int i=0; i<stage; i++)//number
 	{
 		num[i]=rand()%11;
 	}
 
 	char op[4]; int temp;
-	for(int i=0; i<4; i++)
+	for(int i=0; i<4; i++)//operation
 	{
 		op[i]=(rand()%4)+43;
 	}
 
 	int result;
-
-
-	result=num[0]op[0]num[1]
 
 
 	printf("%d %c %d %c %d = %d\n",num[0], op[0], num[1], op[1], num[2], result);
