03996ca69783cd1db5abbe113f6c295218308c8e
Hong JiHyeon <201501668@inu.ac.kr>
2017-12-03 15:22:57 +0900

    if-else

--- func1.c
+++ func1.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define PLUS 1000
+#define MINUS 1001
+#define MULTI 1002
+#define DIVIDE 1003
+
 void func1(int stage)
 {
 	srand(time(NUMLL)); //random_number
 	
 	int num=stage*2+3;
-	int array = (int)malloc(num*sizeof(int))
+	int *array = (int*)malloc(num*sizeof(int));
 
 	for(int i=0; i<num; i++)//number
 	{
 		if(i%2==0)//even number(number)
 		{
-			
+			array[i] = rand()%11;
 		}
-		else
+		else//openration
 		{
-		
+			array[i] = rand()%4 + 1000;
 		}
 	}
+	
 
 	int result;
 
