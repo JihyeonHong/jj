26941311c36e53dd71e440a5e73e285f3562fdfa
RohJaeHee <32832643+RohJaeHee@users.noreply.github.com>
2017-12-08 17:49:55 +0900

    Update game1.c

--- lib/game1.c
+++ lib/game1.c
@@ -1,10 +1,27 @@
 void game1()
 {
-	print(1);
-	int temp = verify(1);
+	stage = 1;
+	init(stage);
 
-	if (result == temp)
-		printf("correct!\n");
-	else
-		printf("wrong!\n");
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			int testresult = makeeq();
+
+			while (testresult < 0)
+				testresult = makeeq();
+
+			print(1);
+			int temp = verify(1);
+
+			if (result == temp)
+				printf("correct!\n");
+			else
+				printf("wrong!\n");
+		}
+
+		stage++;
+		init(stage);
+	}
 }
