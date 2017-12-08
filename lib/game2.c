7801b89f2fb2adce3de73a64d6391c1581f47dbe
JihyeonHong <32832679+JihyeonHong@users.noreply.github.com>
2017-12-08 21:18:22 +0900

    Update game2.c

--- lib/game2.c
+++ lib/game2.c
@@ -3,12 +3,24 @@ void game2()
 	stage = 1;
 	init(stage);
 
+	int life = 3;
+
+
 	int testresult;
 
 	for (int i = 1; i <= 3; i++)
 	{
+		printf("\n<<<<<<<<<<<<<<<<<< Stage%d >>>>>>>>>>>>>>>>>>\n\n", i);
+
 		for (int j = 0; j < 3; j++)
 		{
+			printf("life : ");
+			for (int h = 0; h < life; h++)
+			{
+				printf("♥ ");
+			}
+			printf("\n");
+
 			testresult = makeeq();
 
 			while (testresult < 0)
@@ -24,9 +36,17 @@ void game2()
 			int temp = verify(2);
 
 			if (result == temp)
-				printf("correct!\n");
+				printf("Correct!\n\n");
 			else
-				printf("wrong!\n");
+			{
+				printf("Wrong!\n\n");
+				life--;
+				if (life == 0)
+				{
+					printf("=========== GAME OVER ==========\n");
+					return;
+				}
+			}
 
 			init(stage);
 		}
