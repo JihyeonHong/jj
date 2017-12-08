26156ab6a3601c7fa84a87818695cd2afeec25f7
RohJaeHee <32832643+RohJaeHee@users.noreply.github.com>
2017-12-08 18:14:50 +0900

    Update game3.c

--- lib/game3.c
+++ lib/game3.c
@@ -1,16 +1,37 @@
 void game3()
 {
-	print(3);
+	stage = 1;
+	init(stage);
 
-	char sp;
+	int testresult;
 
-	for (int i = 0; i < or_op_stage; i++)
-		scanf("%c%c", &or_array2[i], &sp);
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			testresult = makeeq();
 
-	int temp = verify(3);
+			while (testresult < 0)
+				testresult = makeeq();
 
-	if (result == temp)
-		printf("correct!\n");
-	else
-		printf("wrong!\n");
+			print(3);
+
+			char sp;
+
+			for (int i = 0; i < or_op_stage; i++)
+				scanf("%c%c", &or_array2[i], &sp);
+
+			int temp = verify(3);
+
+			if (result == temp)
+				printf("correct!\n");
+			else
+				printf("wrong!\n");
+
+			init(stage);
+		}
+
+		stage++;
+		init(stage);
+	}
 }
