374824a2b2163a520c5179a4692b4dd75429a6f3
RohJaeHee <32832643+RohJaeHee@users.noreply.github.com>
2017-12-08 14:36:49 +0900

    adjust verify.c

--- lib/verify.c
+++ lib/verify.c
@@ -1,100 +1,98 @@
-// Verify the answer
-
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#define PLUS 1000
-#define MINUS 1001
-#define MULTI 1002
-#define DIVIDE 1003
-
-int verify()
+int verify(int flag)
 {
 	count = 0;
 
-	for (int h = 0; h < stage; h++)
+	if (flag == 1)
 	{
-		for (int i = 0; i < or_op_stage; i++)
-		{
-			if (or_array2[i] == MULTI)
-			{
-				or_array1[i] = or_array1[i] * or_array1[i + 1];
+		int temp;
+		scanf("%d", &temp);
+		return temp;
+	}
 
-				for (int j = i + 1; j < or_num_stage; j++)
+	else
+	{
+		for (int h = 0; h < stage; h++)
+		{
+			for (int i = 0; i < or_op_stage; i++)
+			{
+				if (or_array2[i] == MULTI)
 				{
-					or_array1[j] = or_array1[j + 1];
+					or_array1[i] = or_array1[i] * or_array1[i + 1];
+
+					for (int j = i + 1; j < or_num_stage; j++)
+					{
+						or_array1[j] = or_array1[j + 1];
+					}
+					for (int j = i + 1; j < or_op_stage; j++)
+					{
+						or_array2[j - 1] = or_array2[j];
+					}
+					or_num_stage--;
+					or_op_stage--;
+					count++;
+					break;
 				}
-				for (int j = i + 1; j < or_op_stage; j++)
+
+				else if (or_array2[i] == DIVIDE)
 				{
-					or_array2[j - 1] = or_array2[j];
+					if ((or_array1[i] < or_array1[i + 1]) || (or_array1[i] % or_array1[i + 1] != 0))
+					{
+						return -1;
+					}
+
+					or_array1[i] = or_array1[i] / or_array1[i + 1];
+					for (int j = i + 1; j < or_num_stage; j++)
+						or_array1[j] = or_array1[j + 1];
+
+					for (int j = i + 1; j < or_op_stage; j++)
+						or_array2[j - 1] = or_array2[j];
+
+					or_num_stage--;
+					or_op_stage--;
+					count++;
+					break;
 				}
-				or_num_stage--;
-				or_op_stage--;
-				count++;
-				break;
 			}
 
-			else if (or_array2[i] == DIVIDE)
+		}
+
+		for (int h = 0; h < stage - count; h++)
+		{
+			for (int i = 0; i < or_op_stage; i++)
 			{
-				if ((or_array1[i] < or_array1[i + 1]) || (or_array1[i] % or_array1[i + 1] != 0))
+				if (or_array2[i] == PLUS)
 				{
-					return -1;
+					or_array1[i] = or_array1[i] + or_array1[i + 1];
+
+					for (int j = i + 1; j < or_num_stage; j++)
+					{
+						or_array1[j] = or_array1[j + 1];
+					}
+					for (int j = i + 1; j < or_op_stage; j++)
+					{
+						or_array2[j - 1] = or_array2[j];
+					}
+					or_num_stage--;
+					or_op_stage--;
+					break;
 				}
 
-				or_array1[i] = or_array1[i] / or_array1[i + 1];
-				for (int j = i + 1; j < or_num_stage; j++)
-					or_array1[j] = or_array1[j + 1];
+				else if (or_array2[i] == MINUS)
+				{
+					or_array1[i] = or_array1[i] - or_array1[i + 1];
+					for (int j = i + 1; j < or_num_stage; j++)
+						or_array1[j] = or_array1[j + 1];
 
-				for (int j = i + 1; j < or_op_stage; j++)
-					or_array2[j - 1] = or_array2[j];
+					for (int j = i + 1; j < or_op_stage; j++)
+						or_array2[j - 1] = or_array2[j];
 
-				or_num_stage--;
-				or_op_stage--;
-				count++;
-				break;
+					or_num_stage--;
+					or_op_stage--;
+					break;
+				}
 			}
 		}
 
+		return or_array1[0];
 	}
-
-	for (int h = 0; h < stage - count; h++)
-	{
-		for (int i = 0; i < or_op_stage; i++)
-		{
-			if (or_array2[i] == PLUS)
-			{
-				or_array1[i] = or_array1[i] + or_array1[i + 1];
-
-				for (int j = i + 1; j < or_num_stage; j++)
-				{
-					or_array1[j] = or_array1[j + 1];
-				}
-				for (int j = i + 1; j < or_op_stage; j++)
-				{
-					or_array2[j - 1] = or_array2[j];
-				}
-				or_num_stage--;
-				or_op_stage--;
-				break;
-			}
-
-			else if (or_array2[i] == MINUS)
-			{
-				or_array1[i] = or_array1[i] - or_array1[i + 1];
-				for (int j = i + 1; j < or_num_stage; j++)
-					or_array1[j] = or_array1[j + 1];
-
-				for (int j = i + 1; j < or_op_stage; j++)
-					or_array2[j - 1] = or_array2[j];
-
-				or_num_stage--;
-				or_op_stage--;
-				break;
-			}
-		}
-	}
-
-	return or_array1[0];
 }
